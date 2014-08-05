1bc14c7dca51f47d1a258597fcb55392ff4f8a6e
neobradley <vic.william2005@gmail.com>
2014-08-05 09:09:27 +0800

    rest sample

--- app.js
+++ app.js
@@ -1,67 +1,42 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/');
-var api = require('./routes/api');
+/**
+ * App.js
+ * - declare all required libraries
+ * - setup all routing
+ * - use restful style
+ * - setup database setting
+ * */
+var http			= require('http');
+var express	= require('express');
+var path			= require('path');
+var restify		= require('express-restify-mongoose');
+var mongoose	= require('mongoose');
+mongoose.connect('mongodb://localhost/todos');
+var db				= mongoose.connection;
+
+var ToDoSchema = new mongoose.Schema({
+	text: { type: String, required: true },
+	done: { type: Boolean, default: false }
+});
+var ToDoModel = mongoose.model('ToDo', ToDoSchema);
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Make our db accessible to our router
-//app.use(function(req,res,next){
-//    req.db = db;
-//    next();
-//});
-
-app.get('/', routes.index);
-app.get('/api/gStore', api.gStore);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.configure(function () {
+	app.set('port', process.env.PORT || 3000);
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.errorHandler());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	restify.serve(app, ToDoModel, {
+		//exclude: 'text,done'
+	});
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(function (req, res) {
+		res.sendfile(path.join(__dirname, 'public/index.html'));
+	});
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
 });
