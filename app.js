99b72e5ff5de9eb406934565f3a1286309d24d3f
neobradley <vic.william2005@gmail.com>
2014-08-10 14:55:41 +0800

    project two db setup for restify

--- app.js
+++ app.js
@@ -5,19 +5,41 @@
  * - use restful style
  * - setup database setting
  * */
-var http		= require('http');
-var express		= require('express');
-var path		= require('path');
+var http			= require('http');
+var express	= require('express');
+var path			= require('path');
 var restify		= require('express-restify-mongoose');
 var mongoose	= require('mongoose');
 mongoose.connect('mongodb://localhost/todos');
-var db			= mongoose.connection;
+var db				= mongoose.connection;
 
 var ToDoSchema = new mongoose.Schema({
 	text: { type: String, required: true },
 	done: { type: Boolean, default: false }
 });
 var ToDoModel = mongoose.model('ToDo', ToDoSchema);
+
+var RestaurantSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	address: { type: String },
+	phone: { type: String },
+	longitude: { type: String },
+	latitude: { type: String },
+	introduction: { type: String },
+	aircon: { type: Boolean },
+	stars: { type: Number },
+});
+var RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+
+var MenuSchema = new mongoose.Schema({
+	restaurantId: { type: mongoose.Schema.Types.ObjectId, required: true },
+	name: { type: String, required: true },
+	price: { type: Number },
+	introduction: { type: String },
+	type: { type: String },
+	remarks: { type: String },
+});
+var MenuModel = mongoose.model('Menu', MenuSchema);
 
 var app = express();
 
@@ -31,10 +53,16 @@ app.configure(function () {
 	restify.serve(app, ToDoModel, {
 		//exclude: 'text,done'
 	});
-	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(function (req, res) {
-		res.sendfile(path.join(__dirname, 'public/index.html'));
+	restify.serve(app, RestaurantModel, {
+		//exclude: 'text,done'
 	});
+	restify.serve(app, MenuModel, {
+		//exclude: 'text,done'
+	});
+//	app.use(express.static(path.join(__dirname, 'public')));
+//	app.use(function (req, res) {
+//		res.sendfile(path.join(__dirname, 'public/index.html'));
+//	});
 });
 
 http.createServer(app).listen(app.get('port'), function () {
